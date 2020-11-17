512bdba550e5a2d43973d58f461b44bb42b3a3de
PedroAzevedo97 <j.pedro.azevedo@tecnico.ulisboa.pt>
2020-11-17 22:11:30 +0000

    test exercise 3

--- circuit.v
+++ circuit.v
@@ -2,16 +2,36 @@
 `include "iob_lib.vh"
 
 module circuit
+#(
+    parameter W=32
+    )
   (
    `INPUT(rst,1),
    `INPUT(clk,1),
    `INPUT(en,1),
-   `INPUT(x,32),
-   `OUTPUT(y,32)
+   `INPUT(x,W),
+   `OUTPUT(y,W)
    );
+   
+   `SIGNAL(cnt, 7) //7-bit counter as iterator
+   
+   `SIGNAL(y_int, W) //internal y
+   `SIGNAL(A, W) //A
+   `SIGNAL(B, W) //B
+   `SIGNAL2OUT(y, y_int) //connect internal y to output
+   
+   //y shift register 
+   `REG_ARE(clk, rst, 1'b0, en & (cnt!=99), A, y_int)
+   `REG_ARE(clk, rst, 1'b0, en & (cnt!=99), B, x)
+   
+    //iteration counter
+   `COUNTER_ARE(clk, rst, cnt!=99, cnt)
 
-   `SIGNAL(acc, 32)
-   `ACC_ARE(clk, rst, 1'b0, en, acc, x)
-   `SIGNAL2OUT(y, acc)
+   //compute next why
+   `COMB 
+    if(A>B) 
+      y_int=A; 
+    else 
+      y_int=B;
    
 endmodule
--- circuit_tb.v
+++ circuit_tb.v
@@ -1,30 +1,39 @@
 `timescale 1ns / 1ps
 `include "iob_lib.vh"
 
+
+`define W 10
+
 module circuit_tb;
 
    `CLOCK(clk, 10)
    `RESET(rst, 1, 10)
    `SIGNAL(en, 1)
-   `SIGNAL(x, 32)
-   `SIGNAL_OUT(y, 32)
+   `SIGNAL(x, `W)
+   `SIGNAL_OUT(y, `W)
 
    integer i;
-   
+
    initial begin
       $dumpfile("circuit.vcd");
       $dumpvars();
       en=0;
       for (i=0; i<100; i=i+1) begin
-         @(posedge clk) #1 x=i; en=1;
-         @(posedge clk) #1 en=0;
+         @(posedge clk) #1 en=1; x={$random}%1000; $display("Number %d \t x value: %d",i+1, x);
       end
+      
+      $display("The maximum value is %d", y);
 
       @(posedge clk) #100 $finish;
 
    end 
 
-   circuit c0
+   circuit 
+   #(
+     .W(`W)
+    ) 
+   
+   c0
      (
       .rst(rst),
       .clk(clk),
@@ -32,6 +41,5 @@ module circuit_tb;
       .x(x),
       .y(y)
       );
-
 
 endmodule
